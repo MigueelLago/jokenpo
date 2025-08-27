dfe5429b4e99f1a2f537d8fed317e86de6e69d4b
miguellago <miguellago@MacBook-Air-de-Miguel.local>
2025-08-27 11:50:08 -0300

    feat: criado enum com as jogadas possiveis e um metodo para verificar quem vence

---
+++ src/main/java/domain/Move.java
@@ -0,0 +1,11 @@
+package domain;
+
+public enum Move {
+    ROCK, PAPER, SCISSORS;
+
+    public boolean beats(Move other) {
+        return (this == ROCK && other == SCISSORS) ||
+               (this == PAPER && other == ROCK) ||
+               (this == SCISSORS && other == PAPER);
+    }
+}
