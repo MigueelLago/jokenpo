12f978d2bb6d000000d857f933c01603fa9be6ef
miguellago <miguellago@MacBook-Air-de-Miguel.local>
2025-08-27 14:27:22 -0300

    change: add metodo toString para nomes em portugues

--- src/main/java/domain/Outcome.java
+++ src/main/java/domain/Outcome.java
@@ -2,5 +2,13 @@ package domain;
 
 public enum Outcome {
 
-    PLAYER1_WIN, PLAYER2_WIN, DRAW
+    PLAYER1_WIN, PLAYER2_WIN, DRAW;
+
+    public String toString() {
+        return switch (this) {
+            case PLAYER1_WIN -> "Player 1 venceu!";
+            case PLAYER2_WIN -> "Player 2 venceu!";
+            case DRAW -> "Empate!";
+        };
+    }
 }
